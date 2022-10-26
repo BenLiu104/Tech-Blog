1f3d5d17fa175fe2a85b2a1bc10def0688da060c
BenLiu <liumanpan1990@hotmail.com>
2022-10-26 00:44:19 -0400

    prepare the seed data

---
+++ seeds/seed.js
@@ -0,0 +1,35 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const postArray = [];
+  for (const post of postData) {
+    const newPost = await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+    postArray.push(newPost);
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: postArray[Math.floor(Math.random() * postArray.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
